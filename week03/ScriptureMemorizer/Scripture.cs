86ee9569a44c8a323a96762a8fbc5a0bba4f7d1e
Yeikob1805 <yeikob.andre1805@gmail.com>
2026-07-18 00:03:24 -0500

    scripture

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,56 @@
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    private Random _random = new Random();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        foreach (string word in text.Split(" "))
+            _words.Add(new Word(word));
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        List<Word> visibleWords = new List<Word>();
+
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+                visibleWords.Add(word);
+        }
+
+        numberToHide = Math.Min(numberToHide, visibleWords.Count);
+
+        for (int i = 0; i < numberToHide; i++)
+        {
+            int index = _random.Next(visibleWords.Count);
+            visibleWords[index].Hide();
+            visibleWords.RemoveAt(index);
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        List<string> words = new List<string>();
+
+        foreach (Word word in _words)
+            words.Add(word.GetDisplayText());
+
+        return $"{_reference.GetDisplayText()} {string.Join(" ", words)}";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+                return false;
+        }
+
+        return true;
+    }
+}
