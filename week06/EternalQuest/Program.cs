cb57d4ed74b9e71b28725e7b1fde206ba6153888
Yeikob1805 <yeikob.andre1805@gmail.com>
2026-08-07 22:22:24 -0500

    creativity

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,4 +1,6 @@
 using System;
-
+// To show creativity and exceed the requirements, I added a level system to the program. 
+// The user starts at Level 1 and you are able to level up for every 1000 points. 
+// I also made the level save and load with the rest of the goal information, in that way you don't loss your progress when the program is closed.
 GoalManager manager = new GoalManager();
 manager.Start();
